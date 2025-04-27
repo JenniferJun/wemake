c6943a821782dd5e96c008c64271b546e2894cb5
JenniferJun <jenniferjun.au@gmail.com>
2025-04-27 14:37:41 +0900

    3.10 Products Routes

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,6 +3,9 @@ import type { MetaFunction } from "react-router";
 import { ProductCard } from "~/features/products/components/product-card";
 import { Button } from "../components/ui/button";
 import { PostCard } from "~/features/community/components/post-card";
+import { IdeaCard } from "~/features/ideas/components/idea-card";
+import { JobCard } from "~/features/jobs/components/job-card";
+import { TeamCard } from "~/features/teams/components/team-card";
 
 export const meta: MetaFunction = () => {
     return [
@@ -28,6 +31,7 @@ export default function HomePage() {
                 </div>
                 {Array.from({ length: 11 }).map((_, index) => (
                     <ProductCard
+                        key={`productId-${index}`}
                         id={`productId-${index}`}
                         name="Product Name"
                         description="Product Description"
@@ -51,12 +55,91 @@ export default function HomePage() {
                 </div>
                 {Array.from({ length: 11 }).map((_, index) => (
                     <PostCard
+                        key={`postId-${index}`}
                         id={`postId-${index}`}
                         title="What is the best productivity tool?"
                         author="Nico"
                         authorAvatarUrl="https://github.com/apple.png"
                         category="Productivity"
                         postedAt="12 hours ago"
+                    />
+                ))}
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div>
+                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                        IdeasGPT
+                    </h2>
+                    <p className="text-xl font-light text-foreground">
+                        Find ideas for your next project.
+                    </p>
+                    <Button variant="link" asChild className="text-lg p-0">
+                        <Link to="/ideas">Explore all ideas &rarr;</Link>
+                    </Button>
+                </div>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <IdeaCard
+                        key={`ideaId-${index}`}
+                        id={`ideaId-${index}`}
+                        title="A startup that creates an AI-powered generated personal trainer, delivering customized fitness recommendations and tracking of progress using a mobile app to track workouts and progress as well as a website to manage the business."
+                        viewsCount={123}
+                        postedAt="12 hours ago"
+                        likesCount={12}
+                        claimed={index % 2 === 0}
+                    />
+                ))}
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+                <div>
+                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                        Latest Jobs
+                    </h2>
+                    <p className="text-xl font-light text-foreground">
+                        Find your dream job.
+                    </p>
+                    <Button variant="link" asChild className="text-lg p-0">
+                        <Link to="/jobs">Explore all jobs &rarr;</Link>
+                    </Button>
+                </div>
+                {Array.from({ length: 11 }).map((_, index) => (
+                    <JobCard
+                        key={`jobId-${index}`}
+                        id={`jobId-${index}`}
+                        company="Tesla"
+                        companyLogoUrl="https://github.com/facebook.png"
+                        companyHq="San Francisco, CA"
+                        title="Software Engineer"
+                        postedAt="12 hours ago"
+                        type="Full-time"
+                        positionLocation="Remote"
+                        salary="$100,000 - $120,000"
+                    />
+                ))}
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+                <div>
+                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                        Find a team mate
+                    </h2>
+                    <p className="text-xl font-light text-foreground">
+                        Join a team looking for a new member.
+                    </p>
+                    <Button variant="link" asChild className="text-lg p-0">
+                        <Link to="/teams">Explore all teams &rarr;</Link>
+                    </Button>
+                </div>
+                {Array.from({ length: 7 }).map((_, index) => (
+                    <TeamCard
+                        key={`teamId-${index}`}
+                        id={`teamId-${index}`}
+                        leaderUsername="lynn"
+                        leaderAvatarUrl="https://github.com/inthetiger.png"
+                        positions={[
+                            "React Developer",
+                            "Backend Developer",
+                            "Product Manager",
+                        ]}
+                        projectDescription="a new social media platform"
                     />
                 ))}
             </div>
