760f3caf50b33afe772fc4b34bb82761beca0fc1
JenniferJun <jenniferjun.au@gmail.com>
2025-05-28 12:58:25 +1000

    7.9 OTP Login-email

--- app/sql/triggers/user_to_profile_trigger.sql
+++ app/sql/triggers/user_to_profile_trigger.sql
@@ -1,7 +1,6 @@
-DROP TRIGGER IF EXISTS user_to_profile_trigger ON auth.users;
+drop function if exists public.handle_new_user() cascade;
 
-
-create or replace function public.handle_new_user()
+create function public.handle_new_user()
 returns trigger
 language plpgsql
 security definer
@@ -18,6 +17,18 @@ begin
                 values (new.id, 'Anonymous', 'mr.' || substr(md5(random()::text), 1, 8), 'developer');
             end if;
         end if;
+    
+         if new.raw_app_meta_data ? 'provider' AND new.raw_app_meta_data ->> 'provider' = 'kakao' then
+            insert into public.profiles (profile_id, name, username, role, avatar)
+            values (new.id, new.raw_user_meta_data ->> 'name', new.raw_user_meta_data ->> 'preferred_username' || substr(md5(random()::text), 1, 5), 'developer', new.raw_user_meta_data ->> 'avatar_url');
+        end if;
+
+
+        if new.raw_app_meta_data ? 'provider' AND new.raw_app_meta_data ->> 'provider' = 'github' then
+            insert into public.profiles (profile_id, name, username, role, avatar)
+            values (new.id, new.raw_user_meta_data ->> 'full_name', new.raw_user_meta_data ->> 'user_name' || substr(md5(random()::text), 1, 5), 'developer', new.raw_user_meta_data ->> 'avatar_url');
+        end if;
+
     end if;
     return new;
 end;
